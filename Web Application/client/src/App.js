cccde1d07bc8582c6546a4158f800f9f21920c00
anirudh <anirudh07.anand@gmail.com>
2022-05-30 12:33:02 +0530

    add register/login with DB store

--- Web Application/client/src/App.js
+++ Web Application/client/src/App.js
@@ -1,42 +1,100 @@
 import React, { useState } from "react";
+import Axios from "axios";
 import "./App.css";
 
 function App() {
+
+  //registration details
+  const [emailIDReg, setEmailIDReg]  = useState('');
+  const [passwordReg, setPasswordReg]  = useState('');
+  const [usernameReg, setUsernameReg]  = useState('');
+  const [picReg, setPicReg]  = useState('');
+
+  //login details
+  const [emailID, setEmailID]  = useState('');
+  const [password, setPassword]  = useState('');
+
+  //login status
+  const [loginStatus, setLoginStatus] = useState('');
+
+  //pass register data to backend
+  const register = () => {
+    Axios.post("http://localhost:3001/register", {
+      username: emailIDReg,
+      password: passwordReg,
+      userAppName: usernameReg,
+      profilePhoto:  picReg,
+    }).then((response) => {
+      console.log(response);
+    });
+  };
+
+  //pass login data to backend
+  const login = () => {
+    Axios.post("http://localhost:3001/login",{
+      username: emailID,
+      password: password,
+    }).then((response) => {
+      if (response.data.message) {
+        setLoginStatus(response.data.message);
+      }
+      else{
+        setLoginStatus(response.data[0].username);
+      }
+    });
+  };
+
   return (
     <div className="App">
       <div className="login">
         <h1>Login to your Account</h1>
         <label>Email-ID: </label>
-        <input type="text" placeholder="abcde123@gmail.com"/>
+        <input type="text" placeholder="abcde123@gmail.com" onChange={(e) => {
+        setEmailID(e.target.value);
+      }}/>
         <br />
         <br />
         <label>Password: </label>
-        <input type="text" placeholder="****"/>
+        <input type="text" placeholder="****" onChange={(e) => {
+          setPassword(e.target.value);
+        }}/>
         <br />
         <br />
-        <button>Login</button>
+        <button onClick={login}>Login</button>
       </div>
+
 
       <div className="registration">
       <h1>Create an Account</h1>
       <label>Email-ID:   </label>
-      <input type="text" />
+      <input type="text" onChange={(e) => {
+        setEmailIDReg(e.target.value);
+      }}/>
       <br />
       <br />
       <label>Password:   </label>
-      <input type="text" />
+      <input type="text" onChange={(e) => {
+        setPasswordReg(e.target.value);
+      }}/>
       <br />
       <br />
       <label>Username: </label>
-      <input type="text" />
+      <input type="text" onChange={(e) => {
+        setUsernameReg(e.target.value);
+      }}/>
       <br />
       <br />
       <label>Profile Pic: </label>
-      <input type="text" />
+      <input type="text" onChange={(e) => {
+        setPicReg(e.target.value);
+      }}/>
       <br />
       <br />
-      <button>Register</button>
+      <button onClick={register}>Register</button>
       </div>
+    
+      <h1>{loginStatus}</h1>
+
     </div>
   );
 }
--- Web Application/server/index.js
+++ Web Application/server/index.js
@@ -1,10 +1,61 @@
 const express = require('express');
-const app = express();
-const db = require("./models");
+const mysql = require("mysql2");
+const cors = require("cors");
 
-db.sequelize.sync().then(() => {
-    app.listen(3001,() => {
-        console.log('server running on port 3001');
-    });    
+const app = express();
+
+//parse the inputs from the front-end
+app.use(express.json());
+app.use(cors());
+
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "123Washington!!",
+    database: "pros_db"
 });
- 
+
+
+app.post('/register', (req, res) => {
+    
+    const username = req.body.username;
+    const password = req.body.password;
+    const userAppName = req.body.userAppName;
+    const profilePhoto = req.body.profilePhoto;
+    db.query(
+    "INSERT INTO users (username, password, userAppName, profilePhoto) VALUES (?,?,?,?)", 
+    [username, password, userAppName, profilePhoto], 
+    (err, result) => {
+        console.log(err);
+    });
+
+});
+
+
+app.post("/login", (req, res) => {
+    
+    const username = req.body.username;
+    const password = req.body.password;
+    db.query(
+    "SELECT * FROM users where username = ? AND password = ?", 
+    [username, password], 
+    (err, result) => {
+        
+        if(err) {
+            //console.log(err);
+            res.send({err:err});
+        }
+        if (result.length > 0) {
+            res.send(result);
+        }
+        else {
+            res.send({message:"Wrong Username & Password Combination"});
+        }
+    });
+
+});
+
+
+app.listen(3001, () => {
+    console.log("server running on port 3001");
+});

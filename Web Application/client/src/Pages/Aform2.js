d052499b32aca5811eafeb08dbca86f340ee6f1c
anirudh <anirudh07.anand@gmail.com>
2022-06-12 19:02:14 +0530

    add faculty acc/rej for form2

---
+++ Web Application/client/src/Pages/Aform2.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import Axios from "axios";
+
+function Aform2() {
+
+
+    let resString = "<table border=\"2px solid\"><tr><th>Name</th><th>Request</th><th>Accept/Reject</th></tr>";
+
+    const [test,setTest] = useState('');
+
+    let navigate = useNavigate();
+    const { state } = useLocation();
+    const { userAppName, profilePhoto, username } = state;
+    
+    const getForm2 = () => {
+        Axios.post("http://localhost:3001/res2").then((response) => {
+            console.log(response);
+            for(let i=0;i<response.data.length;i++) { 
+                resString += "<tr><td>" + response.data[i].name + "</td><td>" + response.data[i].reason + 
+                "</td><td>" + "Accept<input type=\"radio\" name=\"decision" + i +"\" id=\"yes\"/>   Reject<input type=\"radio\" name=\"decision" + i +"\" id=\"no\" />"
+                + "</td></tr>";
+            }
+            resString += "</table>"
+            console.log(resString);
+            document.getElementById("disp").style.display = "block";
+            document.getElementById("disp1").style.display = "block";
+            document.getElementById("disp").innerHTML = resString;
+        });
+    };
+
+    const clearForm2 = () => {
+        Axios.post("http://localhost:3001/clean2").then((response) =>{
+            console.log("Cleaned!");
+            window.alert("Notification sent to students!");
+            navigate("/facultyProfile", { state: {userAppName: userAppName, profilePhoto: profilePhoto, username: username}});
+        })
+    };
+
+    return (
+        <div>
+            <h1>Hostel Leave Form Responses</h1>
+            <button onClick={getForm2}>Get Responses</button>
+            <br />
+            <br />
+            <div align="center">
+            <div style={{display: "None", margin: "10px", fontSize: "16px"}} id="disp"></div>  
+            <br />
+            <button style={{display: "None"} } id="disp1" onClick={clearForm2}>Submit Responses</button>          
+            </div>
+        </div>
+    );
+}
+export default Aform2;

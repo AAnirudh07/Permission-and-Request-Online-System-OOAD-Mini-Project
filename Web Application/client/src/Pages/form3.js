e1a15f88b8984f17faa603fdc64dc087c427a2c6
anirudh <anirudh07.anand@gmail.com>
2022-06-12 07:02:55 +0530

    fix form3.js

--- Web Application/client/src/Pages/form3.js
+++ Web Application/client/src/Pages/form3.js
@@ -3,7 +3,7 @@ import Axios from "axios";
 import "./form.css";
 import { useNavigate } from "react-router-dom";
 
-function form3() {
+function Form3() {
 
     let navigate = useNavigate();
 
@@ -40,60 +40,41 @@ function form3() {
 
 
       return (
-
         <div>
-            <h1 style="font-size: 50px;color: rgb(2, 51, 51);">Hostel Leave Form</h1>
-            <br /><br />
-            <h2 style="font-family:'Lucida Sans';">
-                <div class="container">
-                    <label for="fname">Name *</label>
+        <h1>On-Duty Form</h1>
+        <br /><br />
+            <div className="container">
+                    <label for="fname">Name</label>
                     <input type="text" id="name" name="name" placeholder="Your name.." pattern="' '|[a-zA-Z ]*"
-                        oninvalid="alert('Please enter valid data');" required onChange={(e) => {
-                            setname(e.target.value);}}/>
+                       required />
                     <label for="phoneno">Phone number</label>
                     <input name="contact number" type="number" placeholder="Your mobile no.."
-                       pattern="[0-9]{1,10}" required onChange={(e) => {
-                        setphno(e.target.value);
-                      }}/>
-                    <label for="email">Mail Id</label>
-                    <input type="email" id="email" name="mailid" placeholder="Your mail id.." required onChange={(e) => {
-                        setEmailID(e.target.value);
-                    }} />
+                        pattern="[0-9]{1,10}" required />
+                        <label for="email">Mail Id</label>
+                        <input type="email" id="email" name="mailid" placeholder="Your mail id.." required />
                     <label for="year">Year</label>
-                    <input type="number" id="year" name="year" pattern="[1-4]" 
-                    placeholder="Enter Year" required onChange={(e) => {
-                        setyear(e.target.value);
-                      }} /><br /><br />
-                    <label>Department:</label><br />
-                    <input name="from" id="from" type="date" required onChange={(e) => {
-        setdep(e.target.value);
-      }}/><br />
-                    <label>Date:</label><br />
-                    <input name="to" id="to" type="date" required onChange={(e) => {
-        settdate(e.target.value); }} /><br /><br /><br />
-      <label for="time">Time from</label>
-                <input type="text" id="time" name="time" placeholder="Enter time.." required onChange={(e) => {
-        setftime(e.target.value); }}></input>
-        <label for="time">Time to</label>
-                <input type="text" id="time" name="time" placeholder="Enter time.." required onChange={(e) => {
-        setttime(e.target.value); }}></input>
-                        <label for="reason">Type the reason </label>
-                        <textarea id="reason" name="reason" placeholder="Your Reason here..."
-                            style="height: 100px;" required onChange={(e) => {
-                                setreason(e.target.value);
-                              }}></textarea>
-                        <label>Request letter: <input type="text" name="proof" value="Request letter" required
-                         onChange={(e) => {
-                            setreasonletter(e.target.value);
-                          }}/> </label>
-                        <br /><br />
-                            <input type="submit" value="Submit" />
-                            <input type="reset" />
-                        </div>
-                        </h2>
-                    </div>
-
-                        );
+                    <input type="number" id="year" name="year" pattern="[1-4]" placeholder="Enter Year" required /><br /><br />
+                    <label for="dept">Department</label>
+                    <input type="text" id="dept" name="dept" placeholder="Your Department.." pattern="' '|[a-zA-Z ]*"
+                       required /><br /><br />
+                       <label>On Duty Request Form</label>
+                       <input type="checkbox" name="type" value="Early Leave Form" checked required />
+                       <br /><br />
+                    <label>Date On Duty:</label><br />
+                    <input name="date" id="date" type="date" required /><br />
+                    <label for="time">Starting Time</label>
+                    <input type="text" id="from" name="from" placeholder="Enter from time.." required />
+                    <label for="time">Ending Time</label>
+                    <input type="text" id="to" name="to" placeholder="Enter to time.." required />
+                    <label for="reason">Type the reason </label>
+                    <input type="text" id="reason" name="reason" placeholder="Your Reason here..." required />
+                    <label>Proof Document(mandatory):</label>
+                    <input type="text" name = "proof" value="Proof letter" required /> 
+                    <br /><br />
+                    <input type="submit" value="Submit" onClick={request} />
+                    <input type="reset" />
+            </div>
+      </div>
+      );
 }
-
-                        export default form3;
+export default Form3;

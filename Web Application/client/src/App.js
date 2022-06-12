c24e62370ec4b6040f5184ecc6ec071a8717ad62
anirudh <anirudh07.anand@gmail.com>
2022-06-12 06:36:57 +0530

    fixed form1.js

--- Web Application/client/src/App.js
+++ Web Application/client/src/App.js
@@ -4,24 +4,16 @@ import LoginRegister from "./Pages/LoginRegister";
 import StudentLandingPage from "./Pages/StudentLandingPage";
 import FacultyLandingPage from "./Pages/FacultyLandingPage";
 import ErrorPage from "./Pages/ErrorPage";
-import form1 from "./Pages/form1";
-import form2 from "./Pages/form2";
-import form3 from "./Pages/form3";
-import form4 from "./Pages/form4";
-import form5 from "./Pages/form5";
+import Form1 from "./Pages/Form1";
 function App() {
 
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<LoginRegister />} />
+                <Route path="/" element={<Form1 />} />
                 <Route path="/studentProfile" element={<StudentLandingPage />} />
                 <Route path="/facultyProfile" element={<FacultyLandingPage />} />
-                <Route path="/form1" element={form1}></Route>
-                <Route path="/form2" element={form2}></Route>
-                <Route path="/form3" element={form3}></Route>
-                <Route path="/form4" element={form4}></Route>
-                <Route path="/form5" element={form5}></Route>
+                <Route path="/form1" element={<Form1 />}></Route>
                 <Route path="*" element={<ErrorPage />} />
             </Routes>
         </Router>
--- Web Application/client/src/Pages/form1.js
+++ Web Application/client/src/Pages/form1.js
@@ -3,7 +3,7 @@ import Axios from "axios";
 import "./form.css";
 import { useNavigate } from "react-router-dom";
 
-function form1() {
+function Form1() {
 
     let navigate = useNavigate();
 
@@ -36,55 +36,43 @@ function form1() {
 
 
       return (
-
         <div>
-            <h1 style="font-size: 50px;color: rgb(2, 51, 51);">Hostel Leave Form</h1>
-            <br /><br />
-            <h2 style="font-family:'Lucida Sans';">
-                <div class="container">
-                    <label for="fname">Name *</label>
-                    <input type="text" id="name" name="name" placeholder="Your name.." pattern="' '|[a-zA-Z ]*"
-                        oninvalid="alert('Please enter valid data');" required onChange={(e) => {
-                            setname(e.target.value);}}/>
-                    <label for="phoneno">Phone number</label>
-                    <input name="contact number" type="number" placeholder="Your mobile no.."
-                       pattern="[0-9]{1,10}" required onChange={(e) => {
-                        setphno(e.target.value);
-                      }}/>
-                    <label for="email">Mail Id</label>
-                    <input type="email" id="email" name="mailid" placeholder="Your mail id.." required onChange={(e) => {
-                        setEmailID(e.target.value);
-                    }} />
-                    <label for="year">Year</label>
-                    <input type="number" id="year" name="year" pattern="[1-4]" 
-                    placeholder="Enter Year" required onChange={(e) => {
-                        setyear(e.target.value);
-                      }} /><br /><br />
-                    <label>Date Leaving:</label><br />
-                    <input name="from" id="from" type="date" required onChange={(e) => {
-        setfdate(e.target.value);
-      }}/><br />
-                    <label>Date Returning:</label><br />
-                    <input name="to" id="to" type="date" required onChange={(e) => {
-        settdate(e.target.value);
-      }}/><br /><br /><br />
-                        <label for="reason">Type the reason </label>
-                        <textarea id="reason" name="reason" placeholder="Your Reason here..."
-                            style="height: 100px;" required onChange={(e) => {
-                                setreason(e.target.value);
-                              }}></textarea>
-                        <label>Request letter: <input type="text" name="proof" value="Request letter" 
-                         onChange={(e) => {
-                            setreasonletter(e.target.value);
-                          }}/> </label>
-                        <br /><br />
-                            <input type="submit" value="Submit" />
-                            <input type="reset" />
-                        </div>
-                        </h2>
-                    </div>
+            <h1>Hostel Leave Form</h1>
+            <br />
+            <br />
+            <div className="container">
+              <label for="fname">Name *</label>
+              <input type="text" id="name" name="name" placeholder="Your name.." pattern="' '|[a-zA-Z ]*" oninvalid="alert('Please enter valid data');" required onChange={(e) => {
+                setname(e.target.value);}}/>
+              <label for="phoneno">Phone number</label>
+              <input name="contact number" type="number" placeholder="Your mobile no.." pattern="[0-9]{1,10}" required onChange={(e) => {
+                setphno(e.target.value);}}/>
+              <label for="email">Mail Id</label>
+              <input type="email" id="email" name="mailid" placeholder="Your mail id.." required onChange={(e) => {
+                  setEmailID(e.target.value);}} />
+              <label for="year">Year</label>
+              <input type="number" id="year" name="year" pattern="[1-4]" placeholder="Enter Year" required onChange={(e) => {
+                  setyear(e.target.value);}} />
+              <br />
+              <br />
+              <label for="date_leaving">Date Leaving:</label><br />
+              <input name="from" id="from" type="date" required onChange={(e) => {setfdate(e.target.value);}}/>
+              <br />
+              <label>Date Returning:</label><br />
+              <input name="to" id="to" type="date" required onChange={(e) => {settdate(e.target.value);}}/>
+              <br /><br /><br />
+              <label for="reason">Type the reason </label>
+              <input type="text" id="reason" name="reason" placeholder="Your Reason here..." required onChange={(e) => {
+                    setreason(e.target.value);}} />
+              <label>Request letter: <input type="text" name="proof" value="Request letter" onChange={(e) => {
+                setreasonletter(e.target.value);}}/> </label>
+              <br />
+              <br />
+              <input type="submit" value="Submit" />
+              <input type="reset" />
+            </div>
+        </div>
 
                         );
 }
-
-                        export default form1;
+export default Form1;

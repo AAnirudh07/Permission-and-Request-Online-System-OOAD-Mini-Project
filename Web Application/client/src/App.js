4b04f176a98daac0894685e13676f81314ed12d2
anirudh <anirudh07.anand@gmail.com>
2022-06-12 07:51:21 +0530

    fix form4.js

--- Web Application/client/src/App.js
+++ Web Application/client/src/App.js
@@ -7,12 +7,13 @@ import ErrorPage from "./Pages/ErrorPage";
 import Form1 from "./Pages/Form1";
 import Form2 from "./Pages/Form2";
 import Form3 from "./Pages/Form3";
+import Form4 from "./Pages/Form4";
 function App() {
 
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Form3 />} />
+                <Route path="/" element={<Form4 />} />
                 <Route path="/studentProfile" element={<StudentLandingPage />} />
                 <Route path="/facultyProfile" element={<FacultyLandingPage />} />
                 <Route path="/form1" element={<Form1 />}></Route>
